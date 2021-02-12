71d6da0d56c7d2bef9413c66674194a0562e5106
LongVN99 <LongVn1999vn@yahoo.com>
2021-02-12 16:29:12 +0000

    created flask app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
@@ -26,7 +27,6 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
